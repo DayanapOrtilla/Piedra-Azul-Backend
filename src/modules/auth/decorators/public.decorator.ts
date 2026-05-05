85112ef210b536b381148c0dcc274dd943a7d77f
c43my <wperdomo@unicauca.edu.co>
2026-05-04 19:19:29 -0500

    Rev keycloak

--- src/modules/auth/decorators/public.decorator.ts
+++ src/modules/auth/decorators/public.decorator.ts
@@ -1,4 +1,11 @@
 import { SetMetadata } from '@nestjs/common';
+import { Unprotected } from 'nest-keycloak-connect';
 
 export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const Public = () => {
+  return (target: any, key: string, descriptor?: any) => {
+    SetMetadata(IS_PUBLIC_KEY, true)(target, key, descriptor);
+    Unprotected()(target, key, descriptor);
+  };
+};
